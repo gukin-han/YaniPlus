50b3d08a1850feccff6bd1ec588e447511d20222
Gukin Han <gukin.dev@gmail.com>
2023-10-18 08:02:47 +0900

    Refactor: reconstruct Login component without MUI

--- frontend/src/Components/Signin.jsx
+++ frontend/src/Components/Signin.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import {Box, Button, Container, Grid, Link, TextField, Typography} from "@mui/material";
 
 export default function Signin() {
   const [logInFormData, setLogInFormData] = useState({
@@ -18,73 +17,44 @@ export default function Signin() {
   };
 
   return (
-    <Container
-      sx={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          로그인
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="이메일 주소"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            variant="standard"
-            onChange={handleChange}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="비밀번호"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            variant="standard"
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-300">
+      <div className="p-4 rounded rounded-lg bg-white shadow-xl">
+        <form className="flex flex-col gap-y-4 p-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col justify-between">
+            <label htmlFor="email">이메일</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              required
+              className="border"
+              value={logInFormData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col justify-between">
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              className="border"
+              value={logInFormData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="border">
             로그인
-          </Button>
-        </Box>
-        <Grid container>
-          <Grid item xs>
-            <Link href="#" variant="body2">
-              Forgot password?
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link href="#" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+          </button>
+        </form>
+        <p>
+          아직 회원이 아니신가요?
+          <a href="/register" className="border m-4">
+            회원가입
+          </a>
+        </p>
+      </div>
+    </div>
   );
 }
